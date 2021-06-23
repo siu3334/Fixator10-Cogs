917e7dfc5264aeb2c376222b819e82d330b2fe40
siu3334 <24418520+siu3334@users.noreply.github.com>
2021-06-23 12:02:24 +0530

    re-add forecast command

--- weather/weather.py
+++ weather/weather.py
@@ -1,16 +1,17 @@
 import asyncio
-import datetime
 import json
+from datetime import datetime, timezone
 from textwrap import shorten
 
 import aiohttp
+from aiohttp.client import request
 import discord
 from redbot.core import __version__ as redbot_ver
 from redbot.core import commands
 from redbot.core.config import Config
 from redbot.core.i18n import Translator, cog_i18n, get_locale
 from redbot.core.utils.chat_formatting import error, inline
-from redbot.core.utils.menus import close_menu, menu
+from redbot.core.utils.menus import close_menu, menu, DEFAULT_CONTROLS
 
 WEATHER_STATES = {
     "clear-day": "\N{Black Sun with Rays}",
@@ -73,28 +74,25 @@ class Weather(commands.Cog):
     async def weather(self, ctx, *, place: str):
         """Shows weather in provided place"""
         api_key = (await self.bot.get_shared_api_tokens("forecastio")).get("secret")
+        request_url = f"https://nominatim.openstreetmap.org/search?q={place}&format=jsonv2&addressdetails=1&limit=1"
+        headers = {
+            "Accept-Language": get_locale(),
+            "User-Agent": f"Red-DiscordBot/{redbot_ver} Fixator10-Cogs/Weather/{self.__version__}",
+        }
         async with ctx.typing():
             try:
-                async with self.session.get(
-                    f"https://nominatim.openstreetmap.org/search?q={place}&format=jsonv2&addressdetails=1&limit=1",
-                    headers={
-                        "Accept-Language": get_locale(),
-                        "User-Agent": f"Red-DiscordBot/{redbot_ver} Fixator10-Cogs/Weather/{self.__version__}",
-                    },
-                ) as r:
+                async with self.session.get(request_url, headers=headers) as r:
                     location = await r.json(loads=json.loads)
             except aiohttp.ClientResponseError as e:
-                await ctx.send(
+                return await ctx.send(
                     error(
-                        _("Cannot find a place {}. OSM returned {}").format(
-                            inline(place), e.status
-                        )
+                        _("Cannot find a place {}. OSM returned {}").format(inline(place), e.status)
                     )
                 )
-                return
+
             if not location:
-                await ctx.send(error(_("Cannot find a place {}").format(inline(place))))
-                return
+                return await ctx.send(error(_("Cannot find a place {}").format(inline(place))))
+
             location = location[0]
             latitude = location.get("lat", 0)
             longitude = location.get("lon", 0)
@@ -110,8 +108,8 @@ class Weather(commands.Cog):
                         return await ctx.send(f"https://http.cat/{response.status}")
                     data = await response.json()
             except asyncio.TimeoutError:
-                await ctx.send(error(_("Unable to get data from forecast.io")))
-                return
+                return await ctx.send(error(_("Unable to get data from forecast.io")))
+
         by_hour = data["currently"]
 
         em = discord.Embed(
@@ -122,7 +120,7 @@ class Weather(commands.Cog):
                 latitude, longitude
             ),
             color=await ctx.embed_color(),
-            timestamp=datetime.datetime.utcfromtimestamp(by_hour["time"]),
+            timestamp=datetime.utcfromtimestamp(by_hour["time"]),
         )
         em.set_author(name=_("Powered by Dark Sky"), url="https://darksky.net/poweredby/")
         em.add_field(
@@ -182,6 +180,121 @@ class Weather(commands.Cog):
         )
         close_control = {"❌": close_menu}
         await menu(ctx, [em], close_control)
+
+    @commands.command()
+    @commands.cooldown(1, 1, commands.BucketType.default)
+    @commands.bot_has_permissions(embed_links=True)
+    async def forecast(self, ctx, *, place: str):
+        """Shows forecast for provided place for upto next 24 hours."""
+        api_key = (await self.bot.get_shared_api_tokens("forecastio")).get("secret")
+        request_url = f"https://nominatim.openstreetmap.org/search?q={place}&format=jsonv2&addressdetails=1&limit=1"
+        headers = {
+            "Accept-Language": get_locale(),
+            "User-Agent": f"Red-DiscordBot/{redbot_ver} Fixator10-Cogs/Weather/{self.__version__}",
+        }
+        async with ctx.typing():
+            try:
+                async with self.session.get(request_url, headers=headers) as r:
+                    location = await r.json(loads=json.loads)
+            except aiohttp.ClientResponseError as e:
+                return await ctx.send(
+                    error(
+                        _("Cannot find a place {}. OSM returned {}").format(inline(place), e.status)
+                    )
+                )
+
+            if not location:
+                return await ctx.send(error(_("Cannot find a place {}").format(inline(place))))
+
+            location = location[0]
+            latitude = location.get("lat", 0)
+            longitude = location.get("lon", 0)
+            utc_now = datetime.now(timezone.utc).replace(tzinfo=None).strftime("%Y-%m-%dT%H:%M:%SZ")
+            base_url = f"https://dark-sky.p.rapidapi.com/{latitude},{longitude},{utc_now}"
+            params = {"lang": "en", "units": "si"}
+            headers = {
+                "x-rapidapi-key": api_key,
+                "x-rapidapi-host": "dark-sky.p.rapidapi.com",
+            }
+            try:
+                async with self.session.get(base_url, params=params, headers=headers) as response:
+                    if response.status != 200:
+                        return await ctx.send(f"https://http.cat/{response.status}")
+                    data = await response.json()
+            except asyncio.TimeoutError:
+                return await ctx.send(error(_("Unable to get data from forecast.io")))
+
+        by_day = data["hourly"]
+        pages = []
+        for i in range(0, 23):
+            data = by_day["data"][i]
+            em = discord.Embed(
+                title=_("Weather in {}").format(
+                    shorten(
+                        location.get("display_name", UNKNOWN_EMOJI),
+                        244,
+                        placeholder="…",
+                    )
+                ),
+                description=f"{by_day['summary']}\n"
+                + _("[View on Google Maps](https://www.google.com/maps/place/{},{})").format(
+                    latitude,
+                    longitude,
+                ),
+                color=await ctx.embed_color(),
+                timestamp=datetime.utcfromtimestamp(data["time"]),
+            )
+            em.set_author(name=_("Powered by Dark Sky"), url="https://darksky.net/poweredby/")
+            em.set_footer(text=_(f"Page {i + 1} of {len(by_day['data'])}"))
+            em.add_field(
+                name=_("Summary"),
+                value="{} {}".format(
+                    WEATHER_STATES.get(by_day.get("icon"), UNKNOWN_EMOJI),
+                    by_day.get("summary", "No summary for this day"),
+                ),
+            )
+            em.add_field(
+                name=_("Temperature"),
+                value=f"{data['temperature']} {await self.get_localized_units(ctx, 'temp')}\n"
+                + f"({data['apparentTemperature']}{await self.get_localized_units(ctx, 'temp')})",
+            )
+            em.add_field(
+                name=_("Air pressure"),
+                value="{} {}".format(
+                    data["pressure"], await self.get_localized_units(ctx, "pressure")
+                ),
+            )
+            em.add_field(name=_("Humidity"), value=f"{int(data['humidity'] * 100)}%")
+            em.add_field(
+                name=_("Visibility"),
+                value="{} {}".format(
+                    data["visibility"], await self.get_localized_units(ctx, "distance")
+                ),
+            )
+            em.add_field(
+                name=_("Wind speed"),
+                value="{} {} {}".format(
+                    await self.wind_bearing_direction(data["windBearing"]),
+                    data["windSpeed"],
+                    await self.get_localized_units(ctx, "speed"),
+                ),
+            )
+            em.add_field(name=_("Cloud cover"), value=f"{int(data['cloudCover'] * 100)}%")
+            em.add_field(
+                name=_("Ozone density"),
+                value="{} [DU](https://en.wikipedia.org/wiki/Dobson_unit)".format(data["ozone"]),
+            )
+            em.add_field(name=_("UV index"), value=data["uvIndex"])
+            em.add_field(
+                name=_("Precipitation"),
+                value=_("Probability: {}%\n").format(int(data["precipProbability"] * 100))
+                + _("Intensity: {} {}").format(
+                    int(data["precipIntensity"] * 100),
+                    await self.get_localized_units(ctx, "intensity"),
+                ),
+            )
+            pages.append(em)
+        await menu(ctx, pages, DEFAULT_CONTROLS)
 
     async def get_localized_units(self, ctx: commands.Context, units_type: str):
         """Get translated contextual units for type"""
